85fc12c61dcad24cf1b5d5185fe0c602a3139c95
mitosagi <54105954+mitosagi@users.noreply.github.com>
2023-01-11 06:51:02 +0900

    fix: specify encoding when unzipping

--- util/lib/unzip.js
+++ util/lib/unzip.js
@@ -6,6 +6,7 @@ export default async function unzip(zipPath, outputPath) {
   const zipStream = extractFull(zipPath, outputPath, {
     $bin: path7za,
     overwrite: 'a',
+    method: ['cp=932'], // AviUtl script is encoded in Shift_JIS, so we need to specify the code page as Shift_JIS(932) when unzipping.
   });
 
   return new Promise((resolve) => {
